f51e58bf1557a204960bbd0c44e5229fb2327f20
Chebe97 <joechebe@gmail.com>
2021-08-27 14:08:42 +0000

    Print the various types on the computer compiled on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
-int main()
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
 {
-   char charType;
-   int intType;
-   long int longintType;
-   long long int longlongintType;
-   float floatType;
+  int a;
+  long int b;
+  long long int c;
+  char d;
+  float f;
 
-   /*size of evaluates the size of a variable*/
-   printf("Size of a char: %zu bytes\n", sizeof (charType));
-   printf("Size of an int: %zu bytes\n", sizeof (intType));
-   printf("Size of a long int: %zu bytes\n", sizeof (longintType));
-   printf("Size of a long long int: %zu bytes\n", sizeof (longlonglongintType));
-   printf("Size of a float: %zu bytes\n", sizeof (floatType));
+  
+  printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+  printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+  printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+  printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+  printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
-   return 0;
+  return 0;
 }
