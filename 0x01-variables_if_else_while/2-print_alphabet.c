5d4ff316c2766a490c65da759dc23f85fa9900b1
Chebe97 <joechebe@gmail.com>
2021-08-30 10:10:40 +0000

    Print lowercase letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success) 
+*/
+
+void main()
+{
+  int ch;
+  clrscr();
+  
+  for (ch = 'a'; ch <= 'z'; ch++)
+  
+  putchar(ch);
+  putchar('\n');
+
+  getch ();
+
+  return 0;
+}
+
+  
