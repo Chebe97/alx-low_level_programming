c29a66caa7f0133e951870395752dd7cb0fa0bfb
Chebe97 <joechebe@gmail.com>
2021-08-31 09:42:25 +0000

    Print the word Holberton and add a new line

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,22 +1,21 @@
-#include <stdio.h>
 #include <unistd.h>
-#include "holberton.h"
+#include "stdio.h"
 
 /**
- *main - Check description
  *
- *Description: It prints the word Holberton, followed by a new line
- *Return: 0
+ *
+ *
  */
+
 int main(void)
 {
         char word[9] = "Holberton";
         int i;
 
         for (i = 0; i < 9; i++)
-	  _putchar(word[i]);
-	_putchar('\n');
+                 _putchar(word[i]);
+        _putchar('\n');
 
-	return (0);
+        return (o);
 
 }
